7f527a725c12d46c00963b6652a18fd23975fe46
Mdnishanbiswas <140757763+Mdnishanbiswas@users.noreply.github.com>
2024-02-07 03:39:13 +0600

    Add files via upload

--- mainScreen.dart
+++ mainScreen.dart
@@ -1,30 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/reusableWidgets/Responsive.dart';
+import 'package:provider/provider.dart';
 
-import '../reusableWidgets/appBar.dart';
-import '../reusableWidgets/topBar/topBarFaculty.dart';
-import 'floatingButton/floatingButtonCreateQuiz.dart';
-import 'navigationDrawer/drawerMain.dart';
-import 'showQuiz/mainScreen.dart';
+import '../../providers/createQuizProvider.dart';
+import '../../reusableWidgets/Responsive.dart';
+import '../../reusableWidgets/appBar.dart';
+import '../../reusableWidgets/topBar/topBarFaculty.dart';
+import 'listView.dart';
+import 'submitQuizButton.dart';
+import 'textFieldWidgets.dart';
+import 'toggleButtonForDifficultyLevel.dart';
 
-class FacultyHome extends StatefulWidget {
-  const FacultyHome({Key? key}) : super(key: key);
+class CreateQuiz extends StatefulWidget {
+  const CreateQuiz({Key? key}) : super(key: key);
 
   @override
-  State<FacultyHome> createState() => _FacultyHomeState();
+  State<CreateQuiz> createState() => _CreateQuizState();
 }
 
-class _FacultyHomeState extends State<FacultyHome> {
+class _CreateQuizState extends State<CreateQuiz> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: ResponsiveWidget.isLargeScreen(context)
-          ? PreferredSize(preferredSize: Size(screenWidth(context), 70), child: const TopBarFaculty())
-          : appBarWithLogout(context, "My Quizzes"),
-      floatingActionButton: floatingButtonCreate(context),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      drawer: navigationDrawer(context),
-      body: showQuiz(context),
+          ? PreferredSize(
+              preferredSize: Size(screenWidth(context), 70),
+              child: const TopBarFaculty())
+          : appBarCreate(context, "Create Quiz"),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: screenHeight(context),
+          width: screenWidth(context),
+          child: Consumer<CreateQuizProvider>(
+            builder: (context, providerValue, child) {
+              return Container(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        width: ResponsiveWidget.isSmallScreen(context) ? screenWidth(context) : screenWidth(context)/2,
+                        child: Column(
+                          children: [
+                            textFieldTitle(providerValue),
+                            textFieldDescription(providerValue),
+                            toggleButtonForDifficultyLevel(context),
+                          ],
+                        ),
+                      ),
+                    ),
+                    listViewQuestions(context, providerValue),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: submitQuizButton(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
     );
   }
 }
